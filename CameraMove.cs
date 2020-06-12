a7631c74b2f02e3a6cb63d39ca66408a23230d19
paperam <60968431+paperam@users.noreply.github.com>
2020-06-12 16:02:38 +0900

    Add files via upload

---
+++ CameraMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject target;
+    public float moveSpeed;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public int Speed=1;
+   
+    
+    void Start()
+    {
+       
+    }
+
+  
+    void Update()
+    {
+        //조작
+       if(Input.GetAxisRaw("Horizontal")>0 || Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * Speed *Time.deltaTime,0f,0f));
+        }
+       if (Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw("Vertical") < 0)
+        {
+            transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * Speed *Time.deltaTime,0f));
+        }
+       
+       
+    }
+
+  }
